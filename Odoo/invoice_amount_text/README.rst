be4cccb9ed6b1fcd7a57d8d67344f03e3bedacab
le_dilem <yacine.bensidhoum@gmail.com>
2015-08-06 15:06:52 +0100

    Message de validation

---
+++ Odoo/invoice_amount_text/README.rst
@@ -0,0 +1,16 @@
+Invoice print amout text for Odoo
+===================================
+
+This module prints in letter Invoices
+french Format
+
+Definitions
+-----------
+
+* Fileds: Add a char field.
+* Inherit report_invoice
+
+
+Contributors
+------------
+* Mr. BENSIDHOUM Kheireddine Yacine <yacine.bensidhoum@gmail.com>
---
+++ Odoo/invoice_amount_text/models/account_invoice.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in root directory
+##############################################################################
+
+
+from openerp import models, fields, api, _
+from openerp.tools import amount_to_text_fr
+
+class AccountInvoice(models.Model):
+    _inherit = 'account.invoice'
+
+    @api.one
+    @api.depends('amount_total')
+    def _compute_text(self):
+        self.amount_in_word = amount_to_text_fr(self.amount_total, self.currency_id.name)
+
+    amount_in_word = fields.Char(string='Invoice Number', readonly=True,
+        default=False, copy=False,  compute='_compute_text'
+        )
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
