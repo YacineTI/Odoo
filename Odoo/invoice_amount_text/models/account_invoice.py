997ca93aa0905ef9c8783782249d105ce1a77c89
BENSIDHOUM Kheireddine Yacine <yacine.bensidhoum@gmail.com>
2015-08-08 13:54:31 +0100

    Update account_invoice.py

--- Odoo/invoice_amount_text/models/account_invoice.py
+++ Odoo/invoice_amount_text/models/account_invoice.py
@@ -15,7 +15,7 @@ class AccountInvoice(models.Model):
     def _compute_text(self):
         self.amount_in_word = amount_to_text_fr(self.amount_total, self.currency_id.name)
 
-    amount_in_word = fields.Char(string='Invoice Number', readonly=True,
+    amount_in_word = fields.Char(string='Montant en lettre', readonly=True,
         default=False, copy=False,  compute='_compute_text'
         )
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
